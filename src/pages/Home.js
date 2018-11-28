daefe94c25afb2dc13d533d7caab388f774f4da7
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-28 10:44:06 -0600

    Using colfox font

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,15 @@ const Container = styled.div`
   width: 90%;
   margin: auto;
 `; 
+const H3 = styled.h3`
+  font-family: "Colfax Bold";
+  font-size: 2em;
+`;
 
 function Home({ toggle }) {
   return (
     <Container>
-      {toggle ? 'Stickers' : 'Gifs'}
+      <H3>{toggle ? 'Stickers' : 'Gifs'}</H3>
       <Suspense fallback={<div>Loading....</div>}>
         <Gifs />
       </Suspense>
