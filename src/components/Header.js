e9d356af031f3158f13d4d01191a7e0401808a9b
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-27 18:58:40 -0600

    New component called Switch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import SearchBar from './SearchBar';
 import Toggle from './Toggle';
+
+const NavBar = styled.nav`
+  width: 100%;
+  border-bottom: 1px solid #ccc;
+  background-color: black;
+  padding: 12px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+const I = styled.i`
+  color: white;
+`;
 
 function Header() {
   // const [toggle, useToggle] = useState(false);
   // const toggled = () => useToggle(!toggle);
   return (
-    <>
+    <NavBar>
       <Toggle />
       <SearchBar />
-    </>
+      <I className="fab fa-github"></I>
+    </NavBar>
   );
 }
 export default Header;
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleTerm } from '../actions';
 
+import Switch from './Switch';
+
 function Toggle ({ toggled, toggleTerm }) {
   const render = () => {
     return toggleTerm();
   }
   return (
-    <button onClick={render}>{toggled === false ? 'GIFS' : 'STICKERS'}</button>
+    // <button onClick={render}>{toggled === false ? 'GIFS' : 'STICKERS'}</button>
+    <Switch />
   );
 }
 function mapStateToProps({ toggle }) {
