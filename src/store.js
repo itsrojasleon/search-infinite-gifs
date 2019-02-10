b81cd77642a3522d1e39056c5b75c3857084a4c1
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-10 13:24:08 -0600

    Disable redux-logger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducers from './reducers';
 
-const store = createStore(
-  reducers,
-  applyMiddleware(thunk, logger)
-);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
